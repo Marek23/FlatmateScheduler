40c1ff51a48b7d0aa33aff001552deda4e7c7cc9
Szweda Marek <mszweda@kbj.com.pl>
2020-10-22 11:57:38 +0200

    Kierowanie na stronę główną po zalogowaniu.

---
+++ src/main/java/pl/com/flat/api/HomeApi.java
@@ -0,0 +1,10 @@
+package pl.com.flat.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+class HomeApi {
+	@RequestMapping("/") String index() {return "home";}
+}
