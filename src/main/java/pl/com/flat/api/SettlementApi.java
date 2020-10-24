0ed4df207be38dcf07e14c26b29cc17a82ad15d2
Szweda Marek <mszweda@kbj.com.pl>
2020-10-24 16:39:04 +0200

    Prosty mechanizm zapisu wydatku.

--- src/main/java/pl/com/flat/api/SettlementApi.java
+++ src/main/java/pl/com/flat/api/SettlementApi.java
@@ -3,10 +3,15 @@ package pl.com.flat.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pl.com.flat.model.Settlement;
 import pl.com.flat.repository.SettlementRepository;
+import pl.com.flat.repository.StlTypeRepository;
 import pl.com.flat.security.IFacade;
+import pl.com.flat.util.Text;
 
 @Controller
 @RequestMapping("settlements")
@@ -17,9 +22,33 @@ public class SettlementApi {
 	@Autowired
 	SettlementRepository stlRep;
 
+	@Autowired
+	StlTypeRepository typRep;
+
 	@RequestMapping("/all")
 	public String settlements(Model model) {
 		model.addAttribute("settlements", stlRep.findAll());
+		model.addAttribute("stlTypes",    typRep.findAll());
+
+		model.addAttribute("stl",    new Settlement());
+		model.addAttribute("typeId", new Text());
+
 		return "home/settlements";
+	}
+
+	@PostMapping("/add")
+	public String greetingSubmit(Model model,
+		@ModelAttribute("stl")  Settlement stl,
+		@ModelAttribute("type") Text typeId)
+	{
+		var id   = Long.parseLong(typeId.getValue());
+		var type = typRep.findById(id).get();
+
+		stl.setType(type);
+		stl.setResident(facade.currentResident());
+
+		stlRep.save(stl);
+
+		return settlements(model);
 	}
 }
---
+++ src/main/java/pl/com/flat/util/Text.java
@@ -0,0 +1,8 @@
+package pl.com.flat.util;
+
+import lombok.Data;
+
+@Data
+public class Text {
+	private String value;
+}
