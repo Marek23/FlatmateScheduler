d63d5220d6aa810a438c9141c48d23533396cbdb
Szweda Marek <mszweda@kbj.com.pl>
2021-01-07 13:55:48 +0100

    Kodowanie hasła.

--- src/main/java/pl/com/flat/security/WebSecurityConfigurerAdapterImpl.java
+++ src/main/java/pl/com/flat/security/WebSecurityConfigurerAdapterImpl.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -21,8 +21,8 @@ public class WebSecurityConfigurerAdapterImpl extends WebSecurityConfigurerAdapt
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
 
